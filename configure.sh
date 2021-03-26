d2db2366288b98a616f6753d56ea6899ea1b5600
iatomato <77953022+iatomato@users.noreply.github.com>
2021-03-26 21:14:39 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# A simple bash script for v2ray-core service
+# Written by ISNTL <?>
+
+#script_name=
+#service_name=
+#node_dir=
+#core_dir=
+#logs_null_dir=
+#logs_dir=
+
+cat <<EOF > $install_dir > $add_dir
+#!/bin/bash
+# A simple bash script for v2ray-core service
+# Written by ISNTL <?>
+EOF
+
+cat <<EOF >> $install_dir
+EOF
