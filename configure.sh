fa29592bd6a855fbe09513903e9587579d21e994
ISNTL [Backup Account] <77953022+iatomato@users.noreply.github.com>
2021-07-21 10:51:41 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,41 +1,41 @@
-#!/bin/bash
+# #!/bin/bash
 
-# A simple bash script for v2ray-core service
-# Written by ISNTL <?>
+# # A simple bash script for v2ray-core service
+# # Written by ISNTL <?>
 
-#script_name=
-#service_name=
-#node_dir=
-#core_dir=
-#logs_null_dir=
-#logs_dir=
+# #script_name=
+# #service_name=
+# #node_dir=
+# #core_dir=
+# #logs_null_dir=
+# #logs_dir=
 
-declare prefix_path
-declare cache_path
-declare node_path
-declare config_path
-declare script_name
-declare logs_path
-declare null_path=/dev/null
+# declare prefix_path
+# declare cache_path
+# declare node_path
+# declare config_path
+# declare script_name
+# declare logs_path
+# declare null_path=/dev/null
 
-declare readonly _max=$#
-for (( declare index=1; i <= $_max; ++index ))
-do
-    case "$index" in
-        "prefix_path=*") prefix_path=${index#*=};;
-        "cache_path=*") cache_path=${index#*=};;
-        "node_path=*") node_path=${index#*=} ;;
-        "config_path") config_path=${index#*=} ;;
-        "script_name") script_name=${index#*=} ;;
-        "logs_path") logs_path=${index#*=} ;;
-        "null_path") null_path=${index#*=} ;;
-    esac
-done
+# declare readonly _max=$#
+# for (( declare index=1; i <= $_max; ++index ))
+# do
+#     case "$index" in
+#         "prefix_path=*") prefix_path=${index#*=};;
+#         "cache_path=*") cache_path=${index#*=};;
+#         "node_path=*") node_path=${index#*=} ;;
+#         "config_path") config_path=${index#*=} ;;
+#         "script_name") script_name=${index#*=} ;;
+#         "logs_path") logs_path=${index#*=} ;;
+#         "null_path") null_path=${index#*=} ;;
+#     esac
+# done
 
-readonly perfix_path
-readonly cache_path
-readonly node_path
-readonly config_path
-readonly script_name
-readonly logs_path
-readonly null_path
+# readonly perfix_path
+# readonly cache_path
+# readonly node_path
+# readonly config_path
+# readonly script_name
+# readonly logs_path
+# readonly null_path
